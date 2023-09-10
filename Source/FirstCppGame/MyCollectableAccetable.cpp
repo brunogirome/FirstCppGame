bad17e90022e253322da2cf0df7cd1c4bcded15d
Bruno Girome <brunogirome@gmail.com>
2023-09-09 23:06:13 -0300

    inteface tutorial

---
+++ Source/FirstCppGame/MyCollectableAccetable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCollectableAccetable.h"
+
+// Add default functionality here for any IMyCollectableAccetable functions that are not pure virtual.
